c5eca3e5e950a5b7b9598e37ad5c9070e52c76fa
UzielLH <014430576@ulsaoaxaca.edu.mx>
2024-10-26 14:40:10 -0600

    commit

---
+++ app/Models/productos.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class productos extends Model
+{
+    protected $fillable = ['nombre','departamento','marca','costo','precioVenta','cantidad'];
+}
---
+++ database/migrations/2024_10_25_212736_productos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('departamento');
+            $table->string('marca');
+            $table->float('costo');
+            $table->float('precioVenta');
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
